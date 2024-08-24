466f4caed18edfa5cb1ac9a33d512c4c486a57a4
Sachi2001-HD <sachinthagethmini001@gmail.com>
2024-08-24 18:43:44 +0530

    Created a sketch of the cart by using html and css

---
+++ cart.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Shopping Cart</title>
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- Include user header -->
+<header>
+
+</header>
+
+<section class="products shopping-cart">
+
+   <h3 class="heading">Shopping Cart</h3>
+
+   <div class="box-container">
+
+      <!-- Example Cart Item -->
+      <form action="" method="post" class="box">
+         <input type="hidden" name="cart_id" value="1">
+         <a href="quick_view.php?pid=1" class="fas fa-eye"></a>
+         <img src="uploaded_img/example.jpg" alt="">
+         <div class="name">Product Name</div>
+         <div class="flex">
+            <div class="price">$20.00/-</div>
+            <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
+            <button type="submit" class="fas fa-edit" name="update_qty"></button>
+         </div>
+         <div class="sub-total"> Sub total : <span>$20.00/-</span> </div>
+         <input type="submit" value="Delete Item" onclick="return confirm('Delete this from cart?');" class="delete-btn" name="delete">
+      </form>
+      <!-- End of Example Cart Item -->
+
+
+   </div>
+
+   <div class="cart-total">
+      <p>Grand total : <span>$40.00/-</span></p>
+      <a href="shop.php" class="option-btn">Continue Shopping</a>
+      <a href="cart.php?delete_all" class="delete-btn" onclick="return confirm('Delete all from cart?');">Delete All Items</a>
+      <a href="checkout.php" class="btn">Proceed to Checkout</a>
+   </div>
+
+</section>
+
+<!-- Include footer -->
+<footer>
+
+</footer>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
