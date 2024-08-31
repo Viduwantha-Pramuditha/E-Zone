e7dc3a1605f6a25c750cf2d1d3b10bbb8e297303
Viduwantha-Pramuditha <pramudithaviduwantha@gmail.com>
2024-08-31 08:40:52 +0530

    Create Components

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="admin_login.php"> <i class="fas fa-angle-right"></i> Admin</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us</h3>
+         <a href="tel:0112248154"><i class="fas fa-phone"></i> +94 11 224 8154</a>
+         <a href="tel:0717354881"><i class="fas fa-phone"></i> +94 71 735 4881</a>
+         <a href="mailto:ezone@gmail.com"><i class="fas fa-envelope"></i> ezone@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Colombo, Sri Lanka</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>X</a>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>LinkedIn</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Copyright @ <?= date('Y'); ?> by <span> E-Zone Web Developement Team</span> | All Rights Reserved!</div>
+
+</footer>
--- home.php
+++ home.php
@@ -20,7 +20,7 @@ include 'components/wishlist_cart.php';
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>home</title>
+   <title>Home</title>
 
    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
    
@@ -48,9 +48,9 @@ include 'components/wishlist_cart.php';
             <img src="images/home-img-1.png" alt="">
          </div>
          <div class="content">
-            <span>upto 50% off</span>
-            <h3>latest smartphones</h3>
-            <a href="shop.php" class="btn">shop now</a>
+            <span>Upto 50% Off</span>
+            <h3>Latest Smartphones</h3>
+            <a href="shop.php" class="btn">Shop Now</a>
          </div>
       </div>
 
@@ -59,9 +59,9 @@ include 'components/wishlist_cart.php';
             <img src="images/home-img-2.png" alt="">
          </div>
          <div class="content">
-            <span>upto 50% off</span>
-            <h3>latest watches</h3>
-            <a href="shop.php" class="btn">shop now</a>
+            <span>Upto 50% Off</span>
+            <h3>Latest Watches</h3>
+            <a href="shop.php" class="btn">Shop Now</a>
          </div>
       </div>
 
@@ -70,9 +70,9 @@ include 'components/wishlist_cart.php';
             <img src="images/home-img-3.png" alt="">
          </div>
          <div class="content">
-            <span>upto 50% off</span>
-            <h3>latest headsets</h3>
-            <a href="shop.php" class="btn">shop now</a>
+            <span>Upto 50% Off</span>
+            <h3>Latest Headsets</h3>
+            <a href="shop.php" class="btn">Shop Now</a>
          </div>
       </div>
 
@@ -88,7 +88,7 @@ include 'components/wishlist_cart.php';
 
 <section class="category">
 
-   <h1 class="heading">shop by category</h1>
+   <h1 class="heading">Shop by Category</h1>
 
    <div class="swiper category-slider">
 
@@ -96,42 +96,42 @@ include 'components/wishlist_cart.php';
 
    <a href="category.php?category=laptop" class="swiper-slide slide">
       <img src="images/icon-1.png" alt="">
-      <h3>laptop</h3>
+      <h3>Laptop</h3>
    </a>
 
    <a href="category.php?category=tv" class="swiper-slide slide">
       <img src="images/icon-2.png" alt="">
-      <h3>tv</h3>
+      <h3>Tv</h3>
    </a>
 
    <a href="category.php?category=camera" class="swiper-slide slide">
       <img src="images/icon-3.png" alt="">
-      <h3>camera</h3>
+      <h3>Camera</h3>
    </a>
 
    <a href="category.php?category=mouse" class="swiper-slide slide">
       <img src="images/icon-4.png" alt="">
-      <h3>mouse</h3>
+      <h3>Mouse</h3>
    </a>
 
    <a href="category.php?category=fridge" class="swiper-slide slide">
       <img src="images/icon-5.png" alt="">
-      <h3>fridge</h3>
+      <h3>Fridge</h3>
    </a>
 
    <a href="category.php?category=washing" class="swiper-slide slide">
       <img src="images/icon-6.png" alt="">
-      <h3>washing machine</h3>
+      <h3>Washing Machine</h3>
    </a>
 
    <a href="category.php?category=smartphone" class="swiper-slide slide">
       <img src="images/icon-7.png" alt="">
-      <h3>smartphone</h3>
+      <h3>Smartphone</h3>
    </a>
 
    <a href="category.php?category=watch" class="swiper-slide slide">
       <img src="images/icon-8.png" alt="">
-      <h3>watch</h3>
+      <h3>Watch</h3>
    </a>
 
    </div>
@@ -144,7 +144,7 @@ include 'components/wishlist_cart.php';
 
 <section class="home-products">
 
-   <h1 class="heading">latest products</h1>
+   <h1 class="heading">Latest Products</h1>
 
    <div class="swiper products-slider">
 
@@ -161,7 +161,7 @@ include 'components/wishlist_cart.php';
       <input type="hidden" name="name" value="<?= $fetch_product['name']; ?>">
       <input type="hidden" name="price" value="<?= $fetch_product['price']; ?>">
       <input type="hidden" name="image" value="<?= $fetch_product['image_01']; ?>">
-      <button class="fas fa-heart" type="submit" name="add_to_wishlist"></button>
+      <button class="fas fa-heart" type="submit" name="Add_to_Wishlist"></button>
       <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="fas fa-eye"></a>
       <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
       <div class="name"><?= $fetch_product['name']; ?></div>
@@ -169,12 +169,12 @@ include 'components/wishlist_cart.php';
          <div class="price"><span>$</span><?= $fetch_product['price']; ?><span>/-</span></div>
          <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
       </div>
-      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+      <input type="submit" value="Add to Cart" class="btn" name="add_to_cart">
    </form>
    <?php
       }
    }else{
-      echo '<p class="empty">no products added yet!</p>';
+      echo '<p class="empty">No Products Added Yet!</p>';
    }
    ?>
 
@@ -185,14 +185,6 @@ include 'components/wishlist_cart.php';
    </div>
 
 </section>
-
-
-
-
-
-
-
-
 
 <?php include 'components/footer.php'; ?>
 
